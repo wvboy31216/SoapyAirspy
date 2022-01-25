419b906e92e24acf102e7e7429b101607763d1da
Aaron Bias <84067123+wvboy31216@users.noreply.github.com>
2022-01-25 02:03:33 +0000

    Update Settings.cpp

--- Settings.cpp
+++ Settings.cpp
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Charles J. Cliffe
+ * Copyright (c) 2022 Charles A. Bias
 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
